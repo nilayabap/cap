d7127da5e97c1083870796fab81b1fac0ffcc188
Nilay Maji <nilay.abap@gmail.com>
2026-03-22 15:43:47 +0000

    rest

--- srv/service.js
+++ srv/service.js
@@ -1,4 +1,5 @@
 const cds = require('@sap/cds');
+const req = require('express/lib/request');
 //const { stringify } = require('querystring');
 
 module.exports = cds.service.impl(async function(srv) {
@@ -7,4 +8,42 @@ module.exports = cds.service.impl(async function(srv) {
         //return req.data.input
     return `Hello ${req.data.input}`
     })
+
+    srv.on('addition', req =>{
+        console.log(req.data)
+        let result = req.data.num1 + req.data.num2;
+
+        return result
+    })
+
+    
+    srv.on('MyFunction', req =>{
+        //console.log(req.data)
+        let result = {}
+
+        if (req.data.Category == 1) {
+            result.product = 'BMW',
+            result.price   = '1200',
+            result.location = 'New Delhi'
+        }
+
+        else {
+            result.product = 'Audi',
+            result.price   = '1800',
+            result.location = 'Kolkata',
+            result.price    = [
+                {
+                    "Chesis" : "LTP231456789",
+                    "Engine" : "ENG123456789"
+
+                }
+
+            ]
+
+        }
+        console.log(result)
+        return result;
+    })
+ 
+
 })
